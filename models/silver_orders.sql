e2644fb8d658489aaa975855fd848d0542120fc5
Joshua Walker <joshwalker.oni@gmail.com>
2025-09-16 13:45:44 +0000

    create silver layer

---
+++ models/silver_orders.sql
@@ -0,0 +1,9 @@
+SELECT
+    id,
+    date(date_format(created_at, 'yyyy-MM-dd')) as order_date,
+    user_id,
+    product_id,
+    unit_price,
+    quantity * unit_price as order_amount
+FROM
+{{ ref('bronze_orders' )}}
