45fdeb36f8f9a30cf9666c88b39a05bd084f9e8d
Joshua Walker <joshwalker.oni@gmail.com>
2025-09-17 16:19:40 +0000

    added incremental materialisation into brinze_orders

--- models/bronze/bronze_orders.sql
+++ models/bronze/bronze_orders.sql
@@ -1,4 +1,17 @@
+{{
+    config(
+        materialized='incremental'
+    )
+}}
+
 SELECT
 *
 FROM 
-{{ source('landing', 'orders') }}
+{{ source('landing', 'orders_incramental') }}
+
+{% if is_incremental() %}
+
+
+where created_at > (select coalesce(max(created_at), '1900-01-01') from {{this}} )
+
+{% endif %}
