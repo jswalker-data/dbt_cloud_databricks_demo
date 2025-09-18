f0f865a8c913d56a54be48edc2685755150ba115
Joshua Walker <joshwalker.oni@gmail.com>
2025-09-18 14:25:30 +0000

    changes materialisation for gold models and bronze orders

--- models/bronze/bronze_orders.sql
+++ models/bronze/bronze_orders.sql
@@ -1,18 +1,4 @@
-{{
-    config(
-        materialized='incremental',
-        unique_key= 'id'
-    )
-}}
-
 SELECT
 *
 FROM 
 {{ source('landing', 'orders') }}
-
-{% if is_incremental() %}
-
-
-where created_at > (select coalesce(max(created_at), '1900-01-01') from {{this}} )
-
-{% endif %}
